3d9644eebd06a804be2da12be1977d31f22778b5
Pradip Kharbuja <pradipkharbuja@gmail.com>
2016-04-30 19:52:27 -0500

    Number to Word Converter Completed

--- NumberToWord/src/com/number/NumberToWord.java
+++ NumberToWord/src/com/number/NumberToWord.java
@@ -2,47 +2,28 @@ package com.number;
 
 public class NumberToWord {
 
+	/* Converts from 0 to 999,999,999 */
+
+	private static final String[] units = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+			"Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
+			"Nineteen" };
+	final static private String[] tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty",
+			"Ninety" };
+
 	public static String convert(int number) {
 
-		String[] results = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-				"Eleven", "Tweleve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
-				"Nineteen" };
+		if (number < 20)
+			return units[number];
+		if (number < 100)
+			return (tens[number / 10] + ((number % 10 > 0) ? " " + convert(number % 10) : "")).trim();
+		if (number < 1000)
+			return (units[number / 100] + " Hundred" + ((number % 100 > 0) ? " " + convert(number % 100) : "")).trim();
+		if (number < 1000000)
+			return (convert(number / 1000) + " Thousand" + ((number % 1000 > 0) ? " " + convert(number % 1000) : ""))
+					.trim();
+		return (convert(number / 1000000) + " Million"
+				+ ((number % 1000000 > 0) ? " " + convert(number % 1000000) : "")).trim();
 
-		String[] tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-
-		if (number < 21) {
-			return results[number];
-		}
-
-		String result = "";
-		int count = 0;
-		while (number > 0) {
-			int temp = number % 10;
-
-			if (count == 0) {
-				if (temp > 0)
-					result = results[temp];
-			} else if (count == 1) {
-				if (temp > 1) {
-					result = tens[temp] + " " + result;
-				}
-			} else if (count == 2) {
-				if (temp > 0) {
-					if (!result.trim().equals("")) {
-						result = results[temp] + " Hundred and " + result;
-					} else {
-						result = results[temp] + " Hundred";
-					}
-				}
-			} else if (count == 3) {
-				result = results[temp] + " Thousand " + result;
-			}
-
-			count++;
-			number /= 10;
-		}
-
-		return result.trim();
 	}
 
 }
--- NumberToWord/test/com/number/NumberToWordTest.java
+++ NumberToWord/test/com/number/NumberToWordTest.java
@@ -43,7 +43,7 @@ public class NumberToWordTest {
 	
 	@Test
 	public void testFor149() {
-		assertEquals("One Hundred and Forty Nine", NumberToWord.convert(149));
+		assertEquals("One Hundred Forty Nine", NumberToWord.convert(149));
 	}
 	
 	@Test
@@ -53,7 +53,7 @@ public class NumberToWordTest {
 	
 	@Test
 	public void testFor999() {
-		assertEquals("Nine Hundred and Ninety Nine", NumberToWord.convert(999));
+		assertEquals("Nine Hundred Ninety Nine", NumberToWord.convert(999));
 	}
 	
 	@Test
@@ -62,7 +62,57 @@ public class NumberToWordTest {
 	}
 	
 	@Test
+	public void testFor1008() {
+		assertEquals("One Thousand Eight", NumberToWord.convert(1008));
+	}
+	
+	@Test
 	public void testFor6589() {
-		assertEquals("Six Thousand Five Hundred and Eighty Nine", NumberToWord.convert(6589));
+		assertEquals("Six Thousand Five Hundred Eighty Nine", NumberToWord.convert(6589));
+	}
+	
+	@Test
+	public void testFor10000() {
+		assertEquals("Ten Thousand", NumberToWord.convert(10000));
+	}
+	
+	@Test
+	public void testFor10006() {
+		assertEquals("Ten Thousand Six", NumberToWord.convert(10006));
+	}
+	
+	@Test
+	public void testFor97611() {
+		assertEquals("Ninety Seven Thousand Six Hundred Eleven", NumberToWord.convert(97611));
+	}
+	
+	@Test
+	public void testFor80000(){
+		assertEquals("Eighty Thousand", NumberToWord.convert(80000));
+	}
+	
+	@Test
+	public void testFor100000(){
+		assertEquals("One Hundred Thousand", NumberToWord.convert(100000));
+	}
+	
+	@Test
+	public void testFor800000(){
+		assertEquals("Eight Hundred Thousand", NumberToWord.convert(800000));
+	}
+	
+	@Test
+	public void testFor888888(){
+		assertEquals("Eight Hundred Eighty Eight Thousand Eight Hundred Eighty Eight", NumberToWord.convert(888888));
+	}
+	
+	@Test
+	public void testFor1000000(){
+		assertEquals("One Million", NumberToWord.convert(1000000));
+	}
+	
+	@Test
+	public void testFor999999999(){
+		assertEquals("Nine Hundred Ninety Nine Million Nine Hundred Ninety Nine Thousand Nine Hundred Ninety Nine", NumberToWord.convert(999999999));
 	}
 }
